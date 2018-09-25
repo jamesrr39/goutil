90f6307d22b8c8152c321153dec2e10fec689eb7
James <jr39.dev@gmail.com>
2018-09-25 21:22:58 +0200

    remove dependency

--- matcher/matcher.go
+++ matcher/matcher.go
@@ -1,9 +1,5 @@
 package matcher
 
-import "github.com/jamesrr39/intelligent-backup-store-app/intelligentstore/intelligentstore"
-
-// FIXME: rename package
-
 type Matcher interface {
-	IsIncluded(relativePath intelligentstore.RelativePath) bool
+	IsIncluded(relativePath string) bool
 }
