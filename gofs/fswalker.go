fe8bf10e519b828c3bd01d5130b5426451635b8c
James <jr39.dev@gmail.com>
2019-02-02 19:25:09 +0100

    move fs walker

---
+++ gofs/fswalker.go
@@ -0,0 +1,60 @@
+package fswalker
+
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"sort"
+
+	"github.com/jamesrr39/goutil/gofs"
+)
+
+type WalkOptions struct {
+	FollowSymlinks bool
+}
+
+func Walk(fs gofs.Fs, path string, walkFunc filepath.WalkFunc, options WalkOptions) error {
+	fileInfo, err := fs.Stat(path)
+	if err != nil {
+		return err
+	}
+
+	if fileInfo.IsDir() {
+		dirEntryInfos, err := ioutil.ReadDir(path)
+		if err != nil {
+			return err
+		}
+
+		sort.Slice(dirEntryInfos, func(i int, j int) bool {
+			return dirEntryInfos[i].Name() > dirEntryInfos[j].Name()
+		})
+
+		for _, dirEntryInfo := range dirEntryInfos {
+			err = Walk(fs, filepath.Join(path, dirEntryInfo.Name()), walkFunc, options)
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	if options.FollowSymlinks {
+		isSymlink := (fileInfo.Mode() & os.ModeSymlink) == 1
+		if isSymlink {
+			linkDest, err := fs.Readlink(path)
+			if err != nil {
+				return err
+			}
+			err = Walk(fs, linkDest, walkFunc, options)
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	err = walkFunc(path, fileInfo, nil)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
