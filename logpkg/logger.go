67955f4a6363b2b6b783e611134e2469d8247e33
James <jr39.dev@gmail.com>
2019-03-20 19:31:22 +0100

    pointer receivers on logger

--- logpkg/logger.go
+++ logpkg/logger.go
@@ -36,27 +36,27 @@ type Logger struct {
 	logLevel  LogLevel
 }
 
-func NewLogger(writer io.Writer, logLevel LogLevel) Logger {
-	return Logger{writer, nowFunc, logLevel}
+func NewLogger(writer io.Writer, logLevel LogLevel) *Logger {
+	return &Logger{writer, nowFunc, logLevel}
 }
 
-func (l Logger) Debug(message string, args ...interface{}) {
+func (l *Logger) Debug(message string, args ...interface{}) {
 	l.log(LogLevelDebug, message, args...)
 }
 
-func (l Logger) Info(message string, args ...interface{}) {
+func (l *Logger) Info(message string, args ...interface{}) {
 	l.log(LogLevelInfo, message, args...)
 }
 
-func (l Logger) Warn(message string, args ...interface{}) {
+func (l *Logger) Warn(message string, args ...interface{}) {
 	l.log(LogLevelWarn, message, args...)
 }
 
-func (l Logger) Error(message string, args ...interface{}) {
+func (l *Logger) Error(message string, args ...interface{}) {
 	l.log(LogLevelError, message, args...)
 }
 
-func (l Logger) log(level LogLevel, message string, args ...interface{}) {
+func (l *Logger) log(level LogLevel, message string, args ...interface{}) {
 	if level < l.logLevel {
 		return
 	}
