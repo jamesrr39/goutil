997dfe53e4df899d8a1855746f365b8bea0451ef
James <jr39.dev@gmail.com>
2019-07-28 23:07:42 +0200

    tidy up replacetool

--- replacetool/replacetool-main.go
+++ replacetool/replacetool-main.go
@@ -13,22 +13,41 @@ import (
 	"strings"
 )
 
-const (
-	replaceString       = "errors.WithStack(err)"
-	errorsPkgImportPath = "github.com/pkg/errors"
-)
-
 var usage = func() {
-	fmt.Printf(`a script for replacing "return err" with return %q\n`, replaceString)
+	fmt.Printf(`a script for replacing "return err" with a custom return.
+
+Usage:
+
+replacetool path/to/project errorsx.Wrap(err) github.com/jamesrr39/goutil/errorsx
+
+Where
+- "path/to/project" is the path to your project that you want the replaces to happen inside
+- "errorsx.Wrap(err)" is the new string to replace with
+- "github.com/jamesrr39/goutil/errorsx" is the package name required for the new string
+`)
 }
 
 func main() {
 	shouldReplace := flag.Bool("replace", false, "should replace err with errors.Wrap(err)")
 	flag.Usage = usage
 	flag.Parse()
-	basePath := flag.Arg(0)
 
-	allSuccess, err := run(basePath, *shouldReplace)
+	if len(flag.Args()) < 3 {
+		flag.Usage()
+		os.Exit(1)
+	}
+	basePath := flag.Arg(0)
+	replaceString := flag.Arg(1)
+	errorsPkgImportPath := flag.Arg(2)
+
+	replacer := &Replacer{
+		basePath:            basePath,
+		shouldReplace:       *shouldReplace,
+		replaceString:       replaceString,
+		errorsPkgImportPath: errorsPkgImportPath,
+	}
+
+	allSuccess, err := replacer.run()
 	if err != nil {
 		log.Fatalln(err)
 	}
@@ -38,9 +57,16 @@ func main() {
 	}
 }
 
-func run(basePath string, shouldReplace bool) (allSuccess bool, err error) {
+type Replacer struct {
+	basePath            string
+	shouldReplace       bool
+	replaceString       string
+	errorsPkgImportPath string
+}
+
+func (r *Replacer) run() (allSuccess bool, err error) {
 	allSuccess = true
-	err = filepath.Walk(basePath, func(path string, fileInfo os.FileInfo, err error) error {
+	err = filepath.Walk(r.basePath, func(path string, fileInfo os.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
@@ -53,13 +79,13 @@ func run(basePath string, shouldReplace bool) (allSuccess bool, err error) {
 			return nil
 		}
 
-		relativePath := strings.TrimPrefix(strings.TrimPrefix(path, basePath), string(filepath.Separator))
+		relativePath := strings.TrimPrefix(strings.TrimPrefix(path, r.basePath), string(filepath.Separator))
 
 		if strings.HasPrefix(relativePath, "vendor"+string(filepath.Separator)) || strings.HasPrefix(relativePath, ".git"+string(filepath.Separator)) {
 			return nil
 		}
 
-		ok, err := runFile(path, fileInfo, shouldReplace)
+		ok, err := r.runFile(path, fileInfo, r.shouldReplace)
 		if err != nil {
 			return err
 		}
@@ -73,7 +99,7 @@ func run(basePath string, shouldReplace bool) (allSuccess bool, err error) {
 	return allSuccess, err
 }
 
-func runFile(filePath string, fileInfo os.FileInfo, replace bool) (bool, error) {
+func (r *Replacer) runFile(filePath string, fileInfo os.FileInfo, replace bool) (bool, error) {
 	fileBytes, err := ioutil.ReadFile(filePath)
 	if err != nil {
 		return false, err
@@ -96,17 +122,16 @@ func runFile(filePath string, fileInfo os.FileInfo, replace bool) (bool, error) 
 					nodes = append(nodes, returnStmt)
 				}
 			}
-
 		}
 		return true
 	})
 
 	if len(nodes) == 0 {
-		return false, nil
+		return true, nil
 	}
 
 	if replace {
-		return true, replaceFileContents(filePath, fileInfo, sourceCode, nodes, parsedFile)
+		return true, r.replaceFileContents(filePath, fileInfo, sourceCode, nodes, parsedFile)
 	}
 	for _, node := range nodes {
 		fmt.Printf("found unwrapped err in %s (pos %d-%d)\n", filePath, node.Pos(), node.End())
@@ -114,29 +139,29 @@ func runFile(filePath string, fileInfo os.FileInfo, replace bool) (bool, error) 
 	return false, nil
 }
 
-func replaceFileContents(filePath string, fileInfo os.FileInfo, sourceCode string, nodes []ast.Expr, parsedFile *ast.File) error {
+func (r *Replacer) replaceFileContents(filePath string, fileInfo os.FileInfo, sourceCode string, nodes []ast.Expr, parsedFile *ast.File) error {
 
 	for i := len(nodes) - 1; i >= 0; i-- {
 		node := nodes[i]
-		sourceCode = fmt.Sprintf("%s%s%s", sourceCode[:node.Pos()-1], replaceString, sourceCode[node.End()-1:])
+		sourceCode = fmt.Sprintf("%s%s%s", sourceCode[:node.Pos()-1], r.replaceString, sourceCode[node.End()-1:])
 	}
 
 	containsPkgErrors := false
 	for _, importStmt := range parsedFile.Imports {
-		if importStmt.Path.Value == fmt.Sprintf(`"%s"`, errorsPkgImportPath) {
+		if importStmt.Path.Value == fmt.Sprintf(`"%s"`, r.errorsPkgImportPath) {
 			containsPkgErrors = true
 		}
 	}
 
 	if !containsPkgErrors {
 		if len(parsedFile.Imports) == 0 {
-			return fmt.Errorf("TODO: no imports in %s, please add %q import by hand", filePath, errorsPkgImportPath)
+			return fmt.Errorf("TODO: no imports in %s, please add %q import by hand", filePath, r.errorsPkgImportPath)
 		}
 
 		lastImport := parsedFile.Imports[len(parsedFile.Imports)-1]
 		sourceCode = fmt.Sprintf(`%s
 	"%s"
-%s`, sourceCode[:lastImport.End()], errorsPkgImportPath, sourceCode[lastImport.End():])
+%s`, sourceCode[:lastImport.End()], r.errorsPkgImportPath, sourceCode[lastImport.End():])
 	}
 
 	err := ioutil.WriteFile(filePath, []byte(sourceCode), fileInfo.Mode())
