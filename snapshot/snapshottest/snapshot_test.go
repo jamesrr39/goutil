0e9db878ab5b67781900d5d24d7a48b23b2d9dba
James <jr39.dev@gmail.com>
2020-05-20 18:59:22 +0200

    update snapshot package

--- snapshot/snapshottest/snapshot_test.go
+++ snapshot/snapshottest/snapshot_test.go
@@ -1,11 +1,22 @@
 package snapshottest
 
 import (
+	"image"
+	"image/color"
 	"testing"
 
 	"github.com/jamesrr39/goutil/snapshot"
 )
 
 func TestAPISnapshot(t *testing.T) {
-	snapshot.AssertMatchesSnapshot(t, "Get /api/example", "{\"name\":\"John\"}")
+	snapshot.AssertMatchesSnapshot(t, "Get /api/example", snapshot.NewTextSnapshot("{\"name\":\"John\"}"))
+}
+
+func TestImageSnapshot(t *testing.T) {
+	img := image.NewRGBA(image.Rect(0, 0, 16, 16))
+
+	img.Set(0, 0, color.Black)
+
+	snapshot.AssertMatchesSnapshot(t, "Image example", snapshot.NewImageSnapshot(img))
+
 }
