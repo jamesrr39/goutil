e777e2dfa6af82d5fa23dba2bcb174fe33de3fbd
James <jr39.dev@gmail.com>
2020-10-30 20:47:16 +0100

    client

--- httpextra/client.go
+++ httpextra/client.go
@@ -1,6 +1,14 @@
 package httpextra
 
-import "net/http"
+import (
+	"bytes"
+	"compress/gzip"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"strings"
+)
 
 // Doer is implemented by http.DefaultClient
 type Doer interface {
@@ -13,4 +21,42 @@ type MockDoer struct {
 
 func (d *MockDoer) Do(req *http.Request) (*http.Response, error) {
 	return d.DoFunc(req)
+}
+
+func CheckResponseCode(expected, got int) error {
+	if got != expected {
+		return fmt.Errorf("expected response code %d but got %d", expected, got)
+	}
+
+	return nil
+}
+
+func RemoveGzip(resp *http.Response) (io.ReadCloser, error) {
+	contentEncoding := resp.Header.Get("content-encoding")
+
+	switch strings.ToLower(contentEncoding) {
+	case "":
+		// no compression
+		return resp.Body, nil
+	case "gzip":
+		r, err := gzip.NewReader(resp.Body)
+		if err != nil {
+			return nil, err
+		}
+
+		bb := bytes.NewBuffer(nil)
+		_, err = io.Copy(bb, r)
+		if err != nil {
+			return nil, err
+		}
+
+		err = r.Close()
+		if err != nil {
+			return nil, err
+		}
+
+		return ioutil.NopCloser(bb), nil
+	}
+
+	return nil, fmt.Errorf("unsupported content-encoding: %q", contentEncoding)
 }
