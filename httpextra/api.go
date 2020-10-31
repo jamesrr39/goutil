eec54be6fedaa996872e496c8e0c1d8c2e712d84
James <jr39.dev@gmail.com>
2020-10-31 21:07:17 +0100

    Add DecodeJSONDataResponse method

--- httpextra/api.go
+++ httpextra/api.go
@@ -1,5 +1,24 @@
 package httpextra
 
+import (
+	"encoding/json"
+	"io"
+
+	"github.com/jamesrr39/goutil/errorsx"
+)
+
 type DataResponse struct {
 	Data interface{} `json:"data"`
+}
+
+func DecodeJSONDataResponse(reader io.Reader, dest interface{}) errorsx.Error {
+	d := new(DataResponse)
+
+	err := json.NewDecoder(reader).Decode(&d)
+	if err != nil {
+		return errorsx.Wrap(err)
+	}
+
+	dest = d.Data
+	return nil
 }
