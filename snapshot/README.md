f7ebfe4dda4deb6ff425fe8b696ce0efa7ac9005
James <jr39.dev@gmail.com>
2021-04-17 15:56:10 +0200

    move snapshot testing to new repo

---
+++ snapshot/README.md
@@ -0,0 +1,1 @@
+This package has been moved to a new repository: [go-snapshot-testing](https://github.com/jamesrr39/go-snapshot-testing)
--- snapshot/text_snapshot.go
+++
@@ -1,8 +0,0 @@
-package snapshot
-
-func NewTextSnapshot(text string) *SnapshotType {
-	return &SnapshotType{
-		DataType: SnapshotDataTypeText,
-		Value:    text,
-	}
-}
